981e925b0c44d67dacb5f177f9d4d9b1f3a7e806
Martin Studna <martin.studna2@gmail.com>
2023-05-10 23:16:37 +0200

    file upload

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,8 +4,14 @@
     <textarea id="query"></textarea>
     <div>
       <button @click="submit">Submit</button>
-      <input id="upload-file" type="file" hidden multiple>
+      <button @click="selectFiles">Select Files</button>
+      <input @change="saveFiles" id="select-files" type="file" hidden multiple>
       <button @click="uploadPdfs">Upload PDFs</button>
+    </div>
+    <div>
+      <div v-for="pdf in pdfs" :key="pdf.id">
+        {{ pdf.name }}
+      </div>
     </div>
   </div>
 </template>
@@ -27,8 +33,14 @@ export default defineComponent({
     submit() {
       console.log('submit')
     },
+    saveFiles(e) {
+      this.pdfs = e.target.files
+    },
     uploadPdfs() {
-
+      
+    },
+    selectFiles() {
+      document.getElementById('select-files').click()
     }
   }
 });
